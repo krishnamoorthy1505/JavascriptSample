8a11490d67463cc4372c586a818375b517615a16
krishnamoorthy1505 <krishnamoorthy82@gmail.com>
2021-11-18 11:02:24 +0530

    react c2

--- src/my.js
+++ src/my.js
@@ -3,11 +3,9 @@ import { getSuggestedQuery } from "@testing-library/dom"
 <h1>hi</h1>
 
 hi
-<<<<<<< HEAD
-gcg
-
-getSuggestedQuery
-=======
 
 hi
->>>>>>> d35030087a860cbf98ae7c9d46c15acd2a085144
+1
+2
+3
+4
