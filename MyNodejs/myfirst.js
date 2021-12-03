7361f85f73732275995cfad1bfe0a380f2fa1ed9
krishnamoorthy1505 <krishnamoorthy82@gmail.com>
2021-12-03 21:18:46 +0530

    Node Example
    Signed-off-by: krishnamoorthy1505 <krishnamoorthy82@gmail.com>

--- MyNodejs/myfirst.js
+++ MyNodejs/myfirst.js
@@ -2,3 +2,9 @@ module.exports.myfunc=function myfunc(){
 console.log("hi");
 }
 module.exports.num=x=100;
+/* num=x=10
+module.exports={
+    func:func,
+    num:num,
+    dummy:()=>{console.log("dummy")}
+} */
