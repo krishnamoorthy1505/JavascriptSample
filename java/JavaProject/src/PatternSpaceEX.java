5fd46b5fb54d2f5b879f85245d202198cb88d641
krishnamoorthy1505 <krishnamoorthy82@gmail.com>
2023-07-07 16:45:52 +0530

    commit 1
    Signed-off-by: krishnamoorthy1505 <krishnamoorthy82@gmail.com>

--- java/JavaProject/src/PatternSpaceEX.java
+++ java/JavaProject/src/PatternSpaceEX.java
@@ -5,15 +5,15 @@ public class PatternSpaceEX {
     public static void main(String[] args) {
         int n=8;
         int i,j;
-attP        for(i=1;i<=n;i++){
+        for(i=1;i<=n;i++){
 
             for (j=1;j<=n/2;j++){
                 if(i==j)
                 System.out.print(j);
-               /*  else if(i>4 && j==n-i){
+                else if(i>4 && j==n-i){
                     System.out.print(j);
                 }
-                */ 
+                
                 else
                 System.out.print(" ");
             }
